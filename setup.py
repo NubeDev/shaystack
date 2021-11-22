d256accd0f05cdd20ee17f682ff444f4b3922bfd
Philippe Prados <github@prados.fr>
2021-11-22 10:51:41 +0100

    Try to compile the package

--- setup.py
+++ setup.py
@@ -6,22 +6,32 @@
 #
 # vim: set ts=4 sts=4 et tw=78 sw=4 si:
 import os
-
+from Cython.Build import cythonize
 from setuptools import setup
+from setuptools import Extension
 
 # See setup.cfg
 if 'CFLAGS' not in os.environ:
     os.environ['CFLAGS'] = '-O3'
 
 setup(
-    setup_requires=['pbr','cythonpackage[build]'],
+    setup_requires=['pbr', 'cythonpackage[build]'],
     pbr=True,
-    cythonpackage={
-        "inject_ext_modules": True,
-        "inject_init": True,
-        "remove_source": True,
-        "compile_pyc": True,
-        "optimize": 1,
-        "exclude":[]
-    },
+    # cythonpackage=True,
+    # cythonpackage={
+    #     "inject_ext_modules": True,
+    #     "inject_init": True,
+    #     "remove_source": True,
+    #     "compile_pyc": True,
+    #     "optimize": 1,
+    #     "exclude":[]
+    # },
+    ext_modules=cythonize(
+        [
+            Extension("shaytstack.*", ["shaystack/*.py"]),
+            Extension("shaytstack.providers.*", ["shaystack/providers/*.py"]),
+         ],
+        compiler_directives={'language_level': 3},
+        build_dir="build/cythonpackage",
+    )
 )
